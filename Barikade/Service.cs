c5d61d125efa430bca1e5e6eff4b8b98c5629be6
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2023-06-20 10:48:26 +0900

    v1.1.0
    Faster program, Fixed the path format in the AllowedPaths.ini file.

---
+++ Barikade/Service.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Management;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Barikade
+{
+    /// <summary>
+    /// The main service class.
+    /// </summary>
+    internal partial class Service : ServiceBase
+    {
+        private ManagementEventWatcher processWatcher;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Service"/> class.
+        /// </summary>
+        internal Service()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Start function to set up the event watcher to monitor process creation.
+        /// </summary>
+        protected override void OnStart(string[] args)
+        {
+            WqlEventQuery query = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_Process'");
+            processWatcher = new ManagementEventWatcher(query);
+            processWatcher.EventArrived += ProcessWatcher_EventArrived;
+            processWatcher.Start();
+        }
+
+        /// <summary>
+        /// Stop function to stop and clean up the event watcher.
+        /// </summary>
+        protected override void OnStop()
+        {
+            processWatcher.EventArrived -= ProcessWatcher_EventArrived;
+            processWatcher.Stop();
+            processWatcher.Dispose();
+        }
+
+        /// <summary>
+        /// Processes watcher function.
+        /// </summary>
+        private void ProcessWatcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            try
+            {
+                var targetInstance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+                var processPath = targetInstance["ExecutablePath"].ToString();
+
+                if (!ProcessPath.IsAllowedPath(processPath))
+                {
+                    _ = Task.Run(() => NetworkConnection.Drop(processPath));
+                    _ = Task.Run(() => FileOperation.Delete(processPath));
+
+                    using (Process process = Process.GetProcessById(Convert.ToInt32(targetInstance["ProcessId"])))
+                    {
+                        try {
+                            process.Kill();
+                        }
+                        catch (Exception)
+                        {
+                            // Ignore any exceptions during the process killing
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
