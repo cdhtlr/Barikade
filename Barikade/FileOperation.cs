45bb2d64e5afe128c8f4e2be5c134e99fcec6be4
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2023-06-24 06:45:24 +0900

    v1.2.0
    Fixed a race condition issue that caused high cpu usage

---
+++ Barikade/FileOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Threading;
+
+namespace Barikade
+{
+    /// <summary>
+    /// The file operation class.
+    /// </summary>
+    internal static class FileOperation
+    {
+        /// <summary>
+        /// Function to delete file.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        internal static void Delete(string filePath)
+        {
+            while (File.Exists(filePath))
+            {
+                try
+                {
+                    // Remove the write and delete permissions from the file
+                    FileSecurity fileSecurity = File.GetAccessControl(filePath);
+                    fileSecurity.AddAccessRule(new FileSystemAccessRule(Environment.UserName,
+                        FileSystemRights.Delete | FileSystemRights.Write, AccessControlType.Deny));
+                    File.SetAccessControl(filePath, fileSecurity);
+
+                    // Delete the file without confirmation
+                    File.Delete(filePath);
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ Barikade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Barikade
+{
+    /// <summary>
+    /// The program class.
+    /// </summary>
+    internal static class Program
+    {
+        internal static string[] monitoredPaths;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+            LoadMonitoredPaths();
+            ServiceBase.Run(new ServiceBase[] { new Service() });
+        }
+
+        /// <summary>
+        /// Loads the monitored paths.
+        /// </summary>
+        private static void LoadMonitoredPaths()
+        {
+            string pathConfigFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @".\AllowedPaths.ini");
+            if (!File.Exists(pathConfigFile))
+                // Create a new path.ini file with default directories
+                File.WriteAllLines(pathConfigFile, new string[] { "+C:\\Program Files\\", "+C:\\Program Files (x86)\\", "+C:\\Windows\\" });
+
+            monitoredPaths = File.ReadAllLines(pathConfigFile);
+        }
+    }
+}
