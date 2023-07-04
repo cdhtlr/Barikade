7ea5afd967c7c59e896f5b532a45c469dbddabec
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2023-07-04 17:04:58 +0900

    v1.4.0
    Changelog:
    - Automatically allows Barikade and files in the same folder as it to run
    - Fixed an error because process monitoring did not return the full path of process
    - Fixed the non-recursive path matching issue
    - Added logic for logging the list of deleted files if a Log.txt file exists

---
+++ Barikade/ProcessPath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Management;
+
+namespace Barikade
+{
+    /// <summary>
+    /// The process path class.
+    /// </summary>
+    internal static class ProcessPath
+    {
+        /// <summary>
+        /// Function to get the process path.
+        /// </summary>
+        /// <param name="processId">The process id.</param>
+        /// <returns>A string.</returns>
+        internal static string Get(int processId)
+        {
+            using (var searcher = new ManagementObjectSearcher($"SELECT ExecutablePath FROM Win32_Process WHERE ProcessId = {processId}"))
+            {
+                var obj = searcher.Get().OfType<ManagementObject>().FirstOrDefault();
+                return obj?["ExecutablePath"]?.ToString() ?? string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Function to determine the path is allowed path.
+        /// </summary>
+        /// <param name="processPath">The process path.</param>
+        /// <returns>A bool.</returns>
+        internal static bool IsAllowedPath(string processPath)
+        {
+            return Program.allowedPaths.Any(allowedPath =>
+            {
+                string normalAllowedPath = allowedPath.Substring(1);
+                return (allowedPath.StartsWith("+") && processPath.StartsWith(normalAllowedPath, StringComparison.OrdinalIgnoreCase)) ||
+                    (allowedPath.StartsWith("-") && processPath.Equals(normalAllowedPath + Path.GetFileName(processPath), StringComparison.OrdinalIgnoreCase));
+            });
+        }
+    }
+}
---
+++ Barikade/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Barikade
+{
+    /// <summary>
+    /// The program class.
+    /// </summary>
+    internal static class Program
+    {
+        internal static string[] allowedPaths;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+            LoadAllowedPaths();
+            ServiceBase.Run(new ServiceBase[] { new Service() });
+        }
+
+        /// <summary>
+        /// Function to load the allowed paths.
+        /// </summary>
+        private static void LoadAllowedPaths()
+        {
+            // Initialize the allowedPaths array with current directory of Barikade (non-recursive)
+            // This will automatically allows the Barikade program and other files that are in the same folder as it to run.
+            List<string> paths = new List<string> { "-"+Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory) };
+
+            // Create a new path.ini file with default directories if AllowedPaths.ini not exists
+            string pathConfigFile = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory), "AllowedPaths.ini");
+            if (!File.Exists(pathConfigFile))
+                File.WriteAllLines(pathConfigFile, new string[] { "+C:\\ProgramData\\Microsoft\\Windows Defender\\Platform\\", "+C:\\Program Files\\", "+C:\\Program Files (x86)\\", "+C:\\Windows\\" });
+
+            paths.AddRange(File.ReadAllLines(pathConfigFile));
+
+            allowedPaths = paths.ToArray();
+        }
+
+        /// <summary>
+        /// Function to write message to log file if Log.txt exists.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        internal static void Log(string message)
+        {
+            string logFile = Path.Combine(Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory), "Log.txt");
+
+            if (File.Exists(logFile))
+                File.AppendAllLines(logFile, new string[] { message });
+        }
+    }
+}
